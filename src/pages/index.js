55daa98733cff3eab38b2aabaf37991c75fd942f
ivann-bravo <ibravo1802@gmail.com>
2023-07-08 17:36:20 -0300

    cambio index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export const router = createBrowserRouter([
         path: '/',
         element: <IndexLayout/>,
         children: [
-            { path: '/home', element: <Home/> },
+            { path: '/', element: <Home/> },
         ]
     }
 ])

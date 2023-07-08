f528e8728843a0235aaacaf517cd380d1db796d4
ivann-bravo <ibravo1802@gmail.com>
2023-07-08 15:25:49 -0300

    prueba card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Card from '../../components/card/Card'
 
 export default function Home() {
 
   return (
-    <div>asd</div>
+    <div>
+      <Card/>
+    </div>
   )
 }

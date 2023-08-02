56958781b4de59a9ee02fd2feb2fb4e834027789
Julian <julianete4@gmail.com>
2023-08-02 20:44:57 -0300

    boton para limpiar filtros agregado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,31 +5,47 @@ import NavBar from '../NavBar/NavBar'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import navActions from '../../store/navState/actions'
+import staysActions from '../../store/stays/actions'
+import data from '../../data.json'
 
+const { allStays } = staysActions
 const { renderNav } = navActions
 
 export default function Header() {
   let navRender = useSelector(store => store.navReducer.state)
   let dispatch = useDispatch()
 
-  function openNav(){
-    dispatch(renderNav({state:true}))
+  function openNav() {
+    dispatch(renderNav({ state: true }))
   }
 
   let stays = useSelector(store => store.staysReducer.stays)
   let staysFilter = useSelector(store => store.staysReducer.filter)
+  console.log(staysFilter)
+
+  function clearFilters() {
+    dispatch(allStays({ stays: data, filter: false }))
+  }
 
   return (
     <header>
       {
-        navRender ? <NavBar/> : <><img src={logo} alt='logo'/>
-        <div className='search' onClick={openNav}>
+        navRender ? <NavBar /> : <><img src={logo} alt='logo' />
+          <div className='buttons-containers'>
+            <div className='search' onClick={openNav}>
+              {
+                staysFilter ? <input type='text' name='location' placeholder='Add Location' value={stays[0].city} /> : <input type='text' name='location' placeholder='Add Location' value='' />
+              }
+              <input type='text' name='guests' placeholder='Add Guests' />
+              <i className="fa-solid fa-magnifying-glass loupe"></i>
+            </div>
             {
-              staysFilter ? <input type='text' name='location' placeholder='Add Location' value={stays[0].city}/> : <input type='text' name='location' placeholder='Add Location'/>
+              staysFilter ? <div className='clear-filter' onClick={clearFilters}>
+                <i className="fa-solid fa-rotate-right fa-flip-horizontal"></i>
+                <p>Clear Filters</p>
+              </div> : <></>
             }
-            <input type='text' name='guests' placeholder='Add Guests'/>
-            <i className="fa-solid fa-magnifying-glass loupe"></i>
-        </div></>
+          </div></>
       }
     </header>
   )
